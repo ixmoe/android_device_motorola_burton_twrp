b9f6c476b8870ced674571bd4c4142785a7871fc
Beta Soft <betaxab@gmail.com>
2021-03-28 16:02:59 +0800

    burton: add twrpfastboot cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ BOARD_KERNEL_CMDLINE := \
     reboot=panic_warm \
     firmware_class.path=/vendor/firmware_mnt/image
 # For the love of all that is holy, please do not include this in your ROM unless you really want TWRP to not work correctly!
-BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE += androidboot.fastboot=1 twrpfastboot=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_BOOT_HEADER_VERSION := 2
